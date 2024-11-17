420a9d33bab2639b070ca44fe79f83f82893614e
nguyenanninh1212212 <ninh56085@gmail.com>
2024-11-17 14:39:29 +0700

    first commit

---
+++ src/main/java/com/mycompany/vietpro/Vietpro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vietpro;
+
+/**
+ *
+ * @author An Ninh
+ */
+public class Vietpro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
